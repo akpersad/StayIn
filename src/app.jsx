86af7faa94a8c73ef3c2af36926359f9c310d227
Andrew Persad <akpersad@gmail.com>
2020-03-28 13:36:31 -0400

    Added animate.css

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,16 @@ import React, { Component } from "react";
 import "./styles/main.scss";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+import FormContainer from "./components/formContainer/formContainer";
+
 class App extends Component {
 	render() {
-		return <div>Hello Wo1rld</div>;
+		return (
+			<div>
+				Hello Wo1rld
+				<FormContainer />
+			</div>
+		);
 	}
 }
 
---
+++ src/components/formContainer/formContainer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class formContainer extends Component {
+	render() {
+		return (
+			<div className="animated fadeInRightBig text-center">
+				<p>Watch me fade in!</p>
+			</div>
+		);
+	}
+}
+
+export default formContainer;
