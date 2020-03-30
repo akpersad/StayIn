352ec96b83d8476e11456bc66ec10325fa796a20
Andrew Persad <akpersad@gmail.com>
2020-03-30 18:54:23 -0400

    Render of section 2

--- src/components/secondQuestions/secondQuestions.jsx
+++ src/components/secondQuestions/secondQuestions.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
-
 import PropTypes from "prop-types";
+
+import { questions } from "../computeNumbers/_listOfQuestions";
 
 class SecondQuestions extends Component {
 	constructor() {
 		super();
 		this.state = {
-			selections: []
+			selections: [],
+			question: "",
+			answers: [],
+			list: []
 		};
 
 		this.handleCheckboxClick = this.handleCheckboxClick.bind(this);
 		this.handleButtonClick = this.handleButtonClick.bind(this);
+	}
+
+	componentDidMount() {
+		this.renderQuestions();
 	}
 
 	handleCheckboxClick(event) {
@@ -18,9 +26,8 @@ class SecondQuestions extends Component {
 	}
 
 	handleDisable(event) {
-		const { selections } = this.state;
+		const { selections, list } = this.state;
 		const val = event.currentTarget.value;
-		const ids = ["vehicle1", "vehicle2", "vehicle3", "vehicle4", "vehicle5", "vehicle6"];
 
 		if (event.currentTarget.checked) {
 			selections.push(val);
@@ -32,14 +39,14 @@ class SecondQuestions extends Component {
 		}
 
 		if (selections.length === 0) {
-			for (let i = 0; i < ids.length; i++) {
-				document.querySelector(`#${ids[i]}`).disabled = false;
+			for (let i = 0; i < list.length; i++) {
+				document.querySelector(`#${list[i]}`).disabled = false;
 			}
-		} else if (!selections.includes("None")) {
-			document.querySelector("#vehicle6").disabled = "disabled";
-		} else if (selections.includes("None")) {
-			for (let i = 0; i < ids.length - 1; i++) {
-				document.querySelector(`#${ids[i]}`).disabled = "disabled";
+		} else if (!selections.includes("none")) {
+			document.querySelector("#none").disabled = "disabled";
+		} else if (selections.includes("none")) {
+			for (let i = 0; i < list.length - 1; i++) {
+				document.querySelector(`#${list[i]}`).disabled = "disabled";
 			}
 		}
 	}
@@ -50,78 +57,40 @@ class SecondQuestions extends Component {
 		onCheckboxSelection(selections);
 	}
 
+	renderQuestions() {
+		const {
+			sectionTwo: { question, answers }
+		} = questions;
+		const list = [];
+		const renderArr = answers.map(item => {
+			const key = Object.keys(item)[0];
+			const value = item[key];
+			list.push(key);
+			return (
+				<label key={key} htmlFor={key} className="checkbox-inputs">
+					<input
+						type="checkbox"
+						onClick={this.handleCheckboxClick}
+						id={key}
+						name={key}
+						value={key}
+					/>
+					<span>{value}</span>
+				</label>
+			);
+		});
+		return this.setState({ question, answers: renderArr, list });
+	}
+
 	render() {
+		const { question, answers } = this.state;
+
 		return (
 			<>
 				<div>
-					<span>Please select all that apply</span>
+					<span>{question}</span>
 				</div>
-				<div>
-					<label htmlFor="vehicle1" className="checkbox-inputs">
-						<input
-							type="checkbox"
-							onClick={this.handleCheckboxClick}
-							id="vehicle1"
-							name="vehicle1"
-							value="Bike"
-						/>
-						<span>Bike</span>
-					</label>
-
-					<label htmlFor="vehicle2" className="checkbox-inputs">
-						<input
-							type="checkbox"
-							onClick={this.handleCheckboxClick}
-							id="vehicle2"
-							name="vehicle2"
-							value="Car"
-						/>
-						<span>Car</span>
-					</label>
-
-					<label htmlFor="vehicle3" className="checkbox-inputs">
-						<input
-							type="checkbox"
-							onClick={this.handleCheckboxClick}
-							id="vehicle3"
-							name="vehicle3"
-							value="Bus"
-						/>
-						<span>Bus</span>
-					</label>
-
-					<label htmlFor="vehicle4" className="checkbox-inputs">
-						<input
-							type="checkbox"
-							onClick={this.handleCheckboxClick}
-							id="vehicle4"
-							name="vehicle4"
-							value="Plane"
-						/>
-						<span>Plane</span>
-					</label>
-
-					<label htmlFor="vehicle5" className="checkbox-inputs">
-						<input
-							type="checkbox"
-							onClick={this.handleCheckboxClick}
-							id="vehicle5"
-							name="vehicle5"
-							value="Train"
-						/>
-						<span>Train</span>
-					</label>
-					<label htmlFor="vehicle6" className="checkbox-inputs">
-						<input
-							type="checkbox"
-							onClick={this.handleCheckboxClick}
-							id="vehicle6"
-							name="vehicle6"
-							value="None"
-						/>
-						<span>None of these apply</span>
-					</label>
-				</div>
+				<div>{answers}</div>
 				<div className="submit-button_group">
 					<button type="button">Go Back</button>
 					<button onClick={this.handleButtonClick} type="button">
